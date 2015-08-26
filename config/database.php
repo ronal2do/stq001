bd618faf443729d99dcb98ed8cf670a9b155ca49
Ronaldo Lima <ronal2do@gmail.com>
2015-08-26 10:36:56 -0300

    edited data

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver'      => 'mysql',
-            'host'        => env('DB_HOST', '127.0.0.1'),
+            'host'        => env('DB_HOST', 'localhost'),
             'database'    => env('DB_DATABASE', 'laravel'),
             'username'    => env('DB_USERNAME', 'root'),
-            'password'    => env('DB_PASSWORD', 'chuba3985'),
-         
+            'password'    => env('DB_PASSWORD', 'root'),
+        
 
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
