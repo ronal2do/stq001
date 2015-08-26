5b5629ad7b5906e9112e901a044f5fdb9f5fdceb
Ronaldo Lima <ronal2do@gmail.com>
2015-08-26 14:23:49 -0300

    categ crud

---
+++ app/Http/Controllers/categoriaController.php
@@ -0,0 +1,47 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use \Serverfireteam\Panel\CrudController;
+
+use Illuminate\Http\Request;
+
+class categoriaController extends CrudController{
+
+    public function all($entity){
+        parent::all($entity); 
+
+			$this->filter = \DataFilter::source(new \App\Categoria);
+			$this->filter->add('name', 'Nome', 'text');
+			$this->filter->submit('buscar');
+			$this->filter->reset('resetar');
+			$this->filter->build();
+
+			$this->grid = \DataGrid::source($this->filter);
+			$this->grid->add('id', 'ID');
+			$this->grid->add('name', 'Nome');
+			
+			$this->addStylesToGrid();
+
+  
+                 
+        return $this->returnView();
+    }
+    
+    public function  edit($entity){
+        
+        parent::edit($entity);
+
+        
+	
+			$this->edit = \DataEdit::source(new \App\Categoria());
+
+			$this->edit->label('Edit Category');
+
+			$this->edit->add('name', 'Nome', 'text');
+		
+        return $this->returnEditView();
+    }    
+}
