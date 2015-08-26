acd20789480fdc2dd2f84121893b2cc829027515
Ronaldo Lima <ronal2do@gmail.com>
2015-08-26 10:49:27 -0300

    edited datad3

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
         'mysql' => [
             'driver'      => 'mysql',
             'host'        => env('DB_HOST', 'localhost'),
-            'database'    => env('DB_DATABASE', 'laravel'),
-            'username'    => env('DB_USERNAME', 'root'),
-            'password'    => env('DB_PASSWORD', 'root'),
+            'database'    => env('DB_DATABASE', 'forge'),
+            'username'    => env('DB_USERNAME', 'forge'),
+            'password'    => env('DB_PASSWORD', ''),
         
 
             'charset'   => 'utf8',
