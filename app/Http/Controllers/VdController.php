74d0e8aa2f4fe051b4d7c5d5d9a7e688167aa167
Ronaldo Lima <ronal2do@gmail.com>
2015-08-28 14:59:27 -0300

    vidcategoria fix

--- app/Http/Controllers/VdController.php
+++ app/Http/Controllers/VdController.php
@@ -1,52 +1,129 @@
-<?php 
-
+<?php
 namespace App\Http\Controllers;
-
+use DB;
+use Illuminate\Http\Request;
+use App\Video;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-use \Serverfireteam\Panel\CrudController;
-
-use Illuminate\Http\Request;
-
-class VdController extends CrudController{
-
-    public function all($entity){
-        parent::all($entity); 
-
-			$this->filter = \DataFilter::source(new \App\Posts);
-			$this->filter->add('nome', 'Nome', 'text');
-			$this->filter->add('categoria', 'Categoria', 'text');
-			$this->filter->submit('Buscar');
-			$this->filter->reset('resetar');
-			$this->filter->build();
-
-			$this->grid = \DataGrid::source($this->filter);
-			$this->grid->add('id', 'ID', 'id');
-			$this->grid->add('nome', 'Nome');
-			$this->grid->add('categoria', 'Categoria');
-			$this->grid->add('url', 'Url');
-			$this->grid->add('foto', 'Foto');
-
-			$this->addStylesToGrid();
-                 
-        return $this->returnView();
+use App\VidCategoria;
+class VdController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {   
+        $cor = '#60a69d';
+        $videod = DB::table('videos')
+            ->where('vid_categoria', '=', 0)
+            ->take(4)
+            ->orderBy('id', 'desc')
+            ->get();
+         $videoc = DB::table('videos')
+            ->where('vid_categoria', '=', 1)
+            ->take(4)
+            ->orderBy('id', 'desc')
+            ->get();
+         $videom = DB::table('videos')
+            ->where('vid_categoria', '=', 2)
+            ->take(4)
+            ->orderBy('id', 'desc')
+            ->get();
+        return view('templates/videos', compact('videod','videoc','videom', 'cor' ));
     }
-    
-    public function  edit($entity){
+    public function lista()
+    {   
+        $cor = '#60a69d';
+        $video = DB::table('videos')
+            
+            ->take(10)
+            ->orderBy('id', 'desc')
+            ->get();
         
-        parent::edit($entity);
-       
-        $this->edit = \DataEdit::source(new \App\Posts());
-
-		$this->edit->label('Editar Postagem');
-
-			$this->edit->add('nome', 'Nome', 'text');
-			$this->edit->add('vid_categoria','Categoria','select')->options(\App\VidCategoria::lists("name", "id")->all());
-			$this->edit->add('url', 'Url', 'text')->rule('required'); 
-			$this->edit->add('foto', 'Foto', 'image')->move('img/upload/');
-
-
-       
-        return $this->returnEditView();
-    }    
+        return view('templates/lista', compact('video','cor' ));
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        $vid_categoria = VidCategoria::lists('name');
+        return view('cadastrar.video', compact('vid_categoria'));
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //dd($request->all());
+        $dadosForm = $request->all();
+        //upload
+        $file = $request->file('foto');
+        if( $request->hasFile('foto') && $file->isValid() ){
+            $file->move('img/upload', $file->getClientOriginalName());
+        }
+        $dadosForm['foto'] = $file->getClientOriginalName();
+        $post = Video::create($dadosForm);        
+        
+        return redirect("videos");
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        $post = Video::find($id);
+        $vid_categoria = VidCategoria::lists('name');
+        return view('templates.video', compact('post', 'vid_categoria'));
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        $post = Video::find($id);
+        $vid_categoria = VidCategoria::lists('name');
+      //dd($post);
+        return view('cadastrar.video', compact('post', 'vid_categoria'));
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        
+        $dadosForm = $request->except('_token');
+        Video::where('id',$id)->update($dadosForm);
+        $vid_categoria = VidCategoria::lists('name');
+        return redirect("home");
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        $post = Video::find($id);
+        $post->delete();
+        return redirect("editarvideo");
+    }
 }
---
+++ app/Http/Controllers/VideoController.php
@@ -0,0 +1,41 @@
+<?php 
+namespace App\Http\Controllers;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use \Serverfireteam\Panel\CrudController;
+use Illuminate\Http\Request;
+class VideoController extends CrudController{
+    public function all($entity){
+        parent::all($entity); 
+       
+			$this->filter = \DataFilter::source(new \App\Video);
+			$this->filter->add('Nome', 'Nome', 'text');
+			$this->filter->submit('Buscar');
+			$this->filter->reset('Resetar');
+			$this->filter->build();
+			$this->grid = \DataGrid::source($this->filter);
+			$this->grid->add('nome', 'Nome');
+			$this->grid->add('vid_categoria', 'Categoria');
+			$this->grid->add('url', 'url');
+			$this->grid->add('foto', 'foto');
+			$this->addStylesToGrid();
+  
+                 
+        return $this->returnView();
+    }
+    
+    public function  edit($entity){
+        
+        parent::edit($entity);
+       
+	
+			$this->edit = \DataEdit::source(new \App\Video());
+			$this->edit->label('Editar Videos');
+			$this->edit->add('nome', 'Nome', 'text');
+			$this->edit->add('vid_categoria', 'Categoria', 'select')->options(\App\VidCategoria::lists("name", "id")->all());
+			$this->edit->add('url', 'url', 'text');
+       
+        return $this->returnEditView();
+    } 
+ 
+}
