0dfc9c595dedc9b151c3a0af627151e662a27e71
Ronaldo Lima <ronal2do@gmail.com>
2015-08-31 17:13:19 -0300

    index final cor3

--- resources/views/templates/videos.blade.php
+++ resources/views/templates/videos.blade.php
@@ -55,10 +55,10 @@
 <section class="sect-posts cf bg-lightgrey xl">
             <div class="row">
             @forelse ($videoc as $videoc)  
-               <article class="g-3 cols">
+               <article class="g-3 cols" style="background-color:rgba(255,0,0,0.5);">
                     <a class="venobox box" data-type="youtube" data-overlay="rgba(136,200,211,0.8)" href="{{$videoc->url}}" style="background-image:url(/img/upload/{{$videoc->foto}});" title="{{$videoc->nome}}"><div class="content">
                         <div class="bg" style="background-image:url('img/upload/{{$videoc->foto}}');"></div>
-                        <div class="content" style="background-color:rgba(255,0,0,0.5);">
+                        <div class="content" >
                             <h4>{{$videoc->nome}}</h4>
                             <p class="meta-author"></p>
                         </div>
