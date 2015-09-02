137229239dcc1829567b8dcb1433b2af5eb68da9
Ronaldo Lima <ronal2do@gmail.com>
2015-09-02 16:46:44 -0300

    TEMPLATETRY FIX 2

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -155,21 +155,26 @@
              <nav role="navigation">
                  <ul>
                      
-                     <li><a href="/">Home</a></li>
-                     <li><a href="/saude">Saúde</a></li>
-                     <li><a href="/educacao">Educação</a></li>
-                     <li><a href="/cidadania">Social</a></li>
-                     <li><a href="/infraestrutura">Infraestrutura</a></li>
-                     <li><a href="/meioambiente">Meio Ambiente</a></li>
-                     <li><a href="/esporte">Esporte</a></li>
- 
--                    <li>   </li>
-+                    <li style="padding-right:100px;">   </li>
-                     <li><a href="https://www.facebook.com/prefsbc" title="Facebook" target="_blank"><i class="icon icon-facebooks"></i></a></li>
-                     <li><a href="https://twitter.com/@pref_sbc" title="Twitter" target="_blank"><i class="icon icon-twitters"></i></a></li>
-                     <li><a href="https://www.youtube.com/channel/UCDPNAIYv6IvCkCNiFGDPynw" title="Youtube" target="_blank"><i class="fa fa-youtube"></i></a></li>
-                     <li><a href="http://instagram.com/#" title="Instagram" target="_blank"><i class="icon icon-instagram"></i></a></li>
-             
+                  <li><a href="/">Home</a></li>
+                    <li><a href="/saude">Saúde</a></li>
+                    <li><a href="/educacao">Educação</a></li>
+                    <li><a href="/cidadania">Social</a></li>
+                    <li><a href="/infraestrutura">Infraestrutura</a></li>
+                    <li><a href="/meioambiente">Meio Ambiente</a></li>
+                    <li><a href="/esporte">Esporte</a></li>
+                    <li><a href="/esporte">Cultura</a></li>
+                    <li><a href="/esporte">Lazer</a></li>
+
+                    <li style="padding-right:20px;">   </li>
+                    <li><a href="http://www.saobernardo.sp.gov.br/home" title="Site da Prefeitura" target="_blank"><i class="fa fa-globe"></i></a></li>
+                    <li><a href="https://www.facebook.com/prefsbc" title="Facebook" target="_blank"><i class="icon icon-facebooks"></i></a></li>
+                    <li><a href="https://twitter.com/@pref_sbc" title="Twitter" target="_blank"><i class="icon icon-twitters"></i></a></li>
+                    <li><a href="https://www.youtube.com/channel/UCDPNAIYv6IvCkCNiFGDPynw" title="Youtube" target="_blank"><i class="fa fa-youtube"></i></a></li>
+                    <li><a href="http://instagram.com/#" title="Instagram" target="_blank"><i class="icon icon-instagram"></i></a></li>
+                    <li>
+                    <iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2Fprefsbc&amp;send=false&amp;layout=button_count&amp;width=103&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21&amp;locale=pt_BR" scrolling="no" frameborder="0" style="border:none; overflow:hidden; max-width:126px; height: 28px;padding-top: 8px;" allowtransparency="true">
+                    </iframe>           
+                    </li>
              </ul>
              </nav>
          </div>
