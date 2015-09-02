98629a8d64af1b4ff21f85a83acf76ccd0ca73f8
Ronaldo Lima <ronal2do@gmail.com>
2015-09-02 15:46:07 -0300

    pixel remove d2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -148,8 +148,7 @@
                     <li><a href="https://www.youtube.com/channel/UCDPNAIYv6IvCkCNiFGDPynw" title="Youtube" target="_blank"><i class="fa fa-youtube"></i></a></li>
                     <li><a href="http://instagram.com/#" title="Instagram" target="_blank"><i class="icon icon-instagram"></i></a></li>
                     <li>
-                    <iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2Fprefsbc&amp;send=false&amp;layout=button_count&amp;width=103&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21&amp;locale=pt_BR" scrolling="no" frameborder="0" style="border:none; overflow:hidden; max-width:126px; height: 28px;padding-top: 8px;" allowtransparency="true">
-                    </iframe>           
+                             
                     </li>
             
             </ul>
