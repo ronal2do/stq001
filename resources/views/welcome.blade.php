613865cc75336418d9fa6fef46c76a02bc97f2ee
Ronaldo Lima <ronal2do@gmail.com>
2015-09-02 10:12:30 -0300

    menu css 4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -204,7 +204,7 @@
                         <a href="/saude">
                         <span class="peep-bio">
                             <h2 style="font-size:4em; font-color:#f6acb3;"></h2>
-                            <p style="font-color:#f6acb3;" >Somos o Hospital de Clínicas Municipal e outras realizações que cuidam da prevenção, do atendimento, do tratamento. </p>
+                            <p style="font-color:#f6acb3;" >Somos o Hospital de Clínicas Municipal e outras realizações que cuidam da prevenção, do atendimento, do tratamento. </p><br />
                             <p style="font-style: italic;">Conheça Mais <i class="fa fa-chevron-right"></i></p>
                         </span>
                        <a>
