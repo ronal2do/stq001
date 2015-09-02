a4eadf45574e3611d1af0a5e6e89ffec3326bb62
Ronaldo Lima <ronal2do@gmail.com>
2015-09-02 15:24:42 -0300

    facebook pixel 2

--- resources/views/fb.php
+++ resources/views/fb.php
@@ -13,3 +13,19 @@ fbq('track', 'PageView');
 src="https://www.facebook.com/tr?id=693404664094662&ev=PageView&noscript=1"
 /></noscript>
 <!-- End Facebook Pixel Code -->
+<!-- Facebook Conversion Code for Conclusão de Contato -->
+<script>(function() {
+var _fbq = window._fbq || (window._fbq = []);
+if (!_fbq.loaded) {
+var fbds = document.createElement('script');
+fbds.async = true;
+fbds.src = '//connect.facebook.net/en_US/fbds.js';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(fbds, s);
+_fbq.loaded = true;
+}
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', '6028473525228', {'value':'0.00','currency':'BRL'}]);
+</script>
+<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6028473525228&amp;cd[value]=0.00&amp;cd[currency]=BRL&amp;noscript=1" /></noscript>
