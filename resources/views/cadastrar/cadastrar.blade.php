8a947fcf916f27c18a3c1103c2fc439331eb2d6f
Ronaldo Lima <ronal2do@gmail.com>
2015-09-14 15:46:21 -0300

    termos de uso

--- resources/views/cadastrar/cadastrar.blade.php
+++ resources/views/cadastrar/cadastrar.blade.php
@@ -65,8 +65,11 @@
 <section class="sect-content bg-lightgrey">
  <div class="row">
 <div class="g-10 cols centered"><br />
-<h2>Termos</h2>
-<p>Ao clicar em Publicar você estará concordando com os termos que podem ser lidos abaixo.</p>
+<h2>Licença dos seus conteúdos e propriedade intelectual</h2>
+<p>Ao enviar qualquer conteúdo ou informação para o Site, incluindo textos, tais como comentários, fóruns de discussão, comunidades, enquetes, testes, seção de dúvidas, participação em concurso cultural, jogos, envio de fotografias, ilustrações, vídeos, arquivos de áudio e outros materiais, você declara autorizar, de forma gratuita, não exclusiva, perpétua, global e livre de remuneração, compensação, retenção ou indenização, o uso do conteúdo por nós, por qualquer modalidade e suporte, para publicação, reprodução, transmissão, retransmissão, distribuição, comunicação ao público, edição, adaptação e outras transformações, tradução para qualquer idioma, inclusão em quaisquer outras obras, representação, execução, uso por radiodifusão e outros meios de comunicação, mediante o emprego de qualquer tecnologia, exposição, inclusão em base de dados e quaisquer outras modalidades de utilização existentes ou que venham a ser inventadas no futuro, em quaisquer suportes existentes ou que venham a ser inventados no futuro, autorizando também a criação de obras derivadas e o sublicenciamento do material para uso em outras plataformas de comunicação, sem limite de tempo, território ou número de utilizações. Qualquer eventual omissão não implicará limitação de uso do material por nós.</p>
+<h2>Caso você não concorde com a autorização acima, ou não tenha os direitos necessários para emitir essa autorização com relação a um conteúdo, você não deve enviar o conteúdo para nós.</h2>
+<p>Você só deve enviar pelo Site ou para o Site materiais originais seus. Todos os direitos autorais patrimoniais sobre o material submetido por você continuam sendo seus.</p>
+<p>Não temos qualquer obrigação de uso dos seus conteúdos e nem perderemos o direito de usá-los, ainda que esse direito não seja exercido por nós.</p>
 </div>
 </div>
 </section>
