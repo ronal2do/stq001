46cdc8c58a36edbaaea481f4a67273154bbbecfe
Ronaldo da Silva Lima <ronal2do@gmail.com>
2015-10-30 14:04:27 -0200

    videos 5responsive

--- resources/views/templates/video.blade.php
+++ resources/views/templates/video.blade.php
@@ -2,11 +2,29 @@
 
 @section('content')										
 
+<style type="text/css">
+	
+	.videoWrapper {
+	position: relative;
+	padding-bottom: 56.25%; /* 16:9 */
+	padding-top: 25px;
+	height: 0;
+}
+.videoWrapper iframe {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+}
+</style>
+
+
 <section class="sect-hero center-all purple" style="background-color: #88c8d3;"  >
 <div>
 		<div class="row">
 			<div class="g-10 cols centered">
-				<h1 style="color:#000;">{{$video->nome}}</h1>
+				<h1>{{$video->nome}}</h1>
 			</div>
 	</div>
 </div>
@@ -17,7 +35,9 @@
 
 <div class="row">
 <div class="row center-all"><br />
+<div class="videoWrapper">
 	<iframe width="853" height="480" src="https://www.youtube.com/embed/{{$video->url}}?rel=0" frameborder="0" allowfullscreen></iframe>
+</div>
 </div>
 <br>
 <div class="row center-all">
