1b00ca44390bd71391e0704374b7c5672080f886
Ronaldo da Silva Lima <ronal2do@gmail.com>
2015-10-30 16:53:33 -0200

    vid cat teste 3

--- resources/views/templates/videos.blade.php
+++ resources/views/templates/videos.blade.php
@@ -1,6 +1,7 @@
 @extends('templates.template')
 
-@section('content')										
+@section('content')	
+									
 <section class="sect-hero center-all purple" style="background-color: #88c8d3;"  >
 <div>
 		<div class="row">
@@ -10,21 +11,32 @@
 	</div>
 </div>
 </section>
-<div class="row">
-	<div class="g-8 offset2 cols">
-		
-		</div></div>
-</section>
-<div class="img-grid has-sep">
-  @forelse ($videos as $video) 
-  <a href="/video/{{$video->id}}">
-  	<img class="left" src="http://img.youtube.com/vi/{{$video->url}}/0.jpg" alt="{{$video->nome}}">
-  </a>
-		
-
-  @empty
-	 	<p>Nenhuma postagem</p>
-  @endforelse 
+<div class="culture center-all">
+        <section id="realizacoes" class="sect-work sect-peeps bg-white" >      
+            @forelse ($videos as $video)
+            <div class="peep-wrapd">
+                <div class="row">
+                    <div class="g-6 cols">
+                        <iframe width="480" height="260" src="https://www.youtube.com/embed/{{$video->url}}?rel=0" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                    <div class="g-6 cols">
+                        <span class="peep-name">
+                            <h4>{{$video->nome}}</h4>
+                            
+                                                    
+                        </span>
+                        <span class="peep-bio">
+                            <h4>Link para ver no</h4>
+                            <hr class="sep-thin">
+                            <p>Youtube</p>
+                        </span>
+                    </div>
+                </div>
+            </div>   
+            @empty
+        <p>Nenhumvideo</p>
+    @endforelse 
+            </section> 
 </div>
 
 @include('contato.news') 
