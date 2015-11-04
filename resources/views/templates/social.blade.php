0bddbb6d9f8b69b37667252fb66cbd7f84345c9b
Ronaldo da Silva Lima <ronal2do@gmail.com>
2015-11-04 13:14:07 -0200

    modf 4

--- resources/views/templates/social.blade.php
+++ resources/views/templates/social.blade.php
@@ -1,4 +1,4 @@
-<li><a href="http://www.saobernardo.sp.gov.br/home" title="Site da Prefeitura" target="_blank"><i class="fa fa-globe"></i></a></li>
+<!--<li><a href="http://www.saobernardo.sp.gov.br/home" title="Site da Prefeitura" target="_blank"><i class="fa fa-globe"></i></a></li>-->
 <li><a href="https://www.facebook.com/prefsbc" title="Facebook" target="_blank"><i class="icon icon-facebooks"></i></a></li>
 <li><a href="https://twitter.com/@pref_sbc" title="Twitter" target="_blank"><i class="icon icon-twitters"></i></a></li>
 <li><a href="https://www.youtube.com/channel/UCDPNAIYv6IvCkCNiFGDPynw" title="Youtube" target="_blank"><i class="fa fa-youtube"></i></a></li>
@@ -8,8 +8,8 @@
 </iframe>   
 
 </li>
-<li><img src="{{asset('img/logo_footer.jpg')}}"
+<li><a href="http://www.saobernardo.sp.gov.br/home" title="Site da Prefeitura" target="_blank"><img src="{{asset('img/logo_footer.jpg')}}"
                          style="border: 5px solid white;
                          width:120px; " 
-                         >    </li>
+                         >  </a>  </li>
                          
