d10ce1726db0deff5e19e5e1cbfe8fe3dcea07d3
Sotaque <sotaque@SOTMAC07.local>
2015-11-05 13:50:19 -0200

    öutro mac 10

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -40,7 +40,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title">#<b>somos</b>sbc.</div>
             </div>
         </div>
     </body>
